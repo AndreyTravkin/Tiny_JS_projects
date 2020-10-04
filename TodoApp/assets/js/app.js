ac67dadc6ca6435c770bc3d7bb530f33efb1bdf4
Andrei Travkin <andreytravkin5@gmail.com>
2020-10-04 05:52:41 -0500

    todo list done

--- TodoApp/assets/js/app.js
+++ TodoApp/assets/js/app.js
@@ -14,6 +14,29 @@
 //     }   
 // });
 
-$('li').click(function(){
+$('ul').on("click", 'li', function(){
    $(this).toggleClass("completed");
 });
+
+// click on X to delete to-do
+
+$('ul').on("click", "span", function(e) {
+    $(this).parent().fadeOut(600, function() {
+        $(this).remove(); 
+    });
+    e.stopPropagation();
+})
+
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+      let todoText = $(this).val();
+      $(this).val("");
+
+      $('ul').append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
